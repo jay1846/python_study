613952d7269f90fef3c5a32bf087cce502225cb9
jay1846 <jay9809012@gmail.com>
2026-06-09 21:55:38 +0200

    simple first project by using simple functions

---
+++ Day1/project1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("which city did you grow up? \n")
+pet_name = input("what is your pet's name? \n")
+print("Your band name could be: " + city + " " + pet_name)
