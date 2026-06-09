3bea61b46c3c637ae61b7e5f96ad9a39a2049fe5
jay1846 <jay9809012@gmail.com>
2026-06-09 21:54:42 +0200

    variables

---
+++ Day1/variable.py
@@ -0,0 +1,17 @@
+# assigns value into variable, save data in variable
+# name = input("what is your name?")
+# print(name)
+
+#name ="Jack"
+# print(name)
+
+# name="Angela"
+# print(name)
+
+# 1. check the length of the user input
+print(len(input("what is your name: ")))
+
+# 2. split everything into variables
+username = input("What is your name? ")
+length = len(username)
+print(length)
