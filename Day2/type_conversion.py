f18392ccd0ef8300dd2034e9cef5d95189a0fbf6
jay1846 <jay9809012@gmail.com>
2026-06-10 10:47:27 +0200

    type conversion

---
+++ Day2/type_conversion.py
@@ -0,0 +1,17 @@
+# add string
+print("123" + "456")
+
+# through type conversion we get calculated number
+print(int("123") + int("456"))
+
+# convert version
+# int()
+# float()
+# str()
+# bool()
+
+
+# through type we should always check data type to concetenate
+user_name = input("Enter your name")
+name_length = len(user_name)
+print("Number of letters in you name: " + str(name_length))
