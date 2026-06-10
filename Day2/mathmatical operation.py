3748ea19ff9b32bed00ecdea964d44f0ffffbf20
jay1846 <jay9809012@gmail.com>
2026-06-10 10:54:27 +0200

    mathmatical operation

---
+++ Day2/mathmatical operation.py
@@ -0,0 +1,13 @@
+print("My age" + str(123))
+print(123 + 456)
+print(7-3)
+print(5*3)
+print(6/3)      # implicit type casting -> float
+print(6//3)     # interger value
+
+# careful by using // in divide 
+print(5/3)
+print(5//3)         # under floating point will be ignored
+
+print(3**4)
+
