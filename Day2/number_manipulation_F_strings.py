6f78d7ff0bc8718df2b3c684a23dda4d83f51c36
jay1846 <jay9809012@gmail.com>
2026-06-10 11:02:51 +0200

    function round and usage of f strings

---
+++ Day2/number_manipulation_F_strings.py
@@ -0,0 +1,20 @@
+bmi = (84 / 1.65 ** 2)
+print(bmi)
+
+print(int(bmi))
+print(round(bmi))
+
+print(round(bmi,2))
+
+score = 0
+
+# user scores a point
+score += 1
+print(score)
+
+# f - strings
+# print("your score is " + str(score))
+score = 90
+height = 1.833
+weight = 84
+print(f"Your score is {score} , height is {height} and weight is {weight}")
