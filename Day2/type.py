e3e224b4f8930d55e32e61c32f2c3a94f3161178
jay1846 <jay9809012@gmail.com>
2026-06-10 10:38:55 +0200

    learn type of 'type'

---
+++ Day2/type.py
@@ -0,0 +1,15 @@
+
+#len("Hello")        ## won't work because len function function with integer
+
+# type of string
+print(type("Hello"))
+
+# type of integer
+print(type(123))
+
+# type of float
+print(type(3.17393))
+
+# type of Boolean
+print(type(True))
+print(type(False))
