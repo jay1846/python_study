ba5f8176cdd316224e07be9bc74b87da1cdfefc0
jay1846 <jay9809012@gmail.com>
2026-06-10 11:22:14 +0200

    feat: by assigning we could determine data type which makes us comforatable not to change data type every time

--- Day2/project2(calculator).py
+++ Day2/project2(calculator).py
@@ -1,8 +1,9 @@
 print("welcome to the tip calculator")
-bill = input("What was the total bill? ")
-tip = input("How much tip do you want to give (10/12/15): ")
-divide_people = input("How many people to split the bill? ")
+bill = float(input("What was the total bill? "))
+tip = int(input("How much tip do you want to give (10/12/15): "))
+divide_people = int(input("How many people to split the bill? "))
 
-total_bill = float(bill)*float(int(tip)/100) + float(bill)
-each_bill = total_bill / int(divide_people)
-print("each people should pay: " + str(round(each_bill,2)))
+total_bill = (bill * (tip/100)) + bill
+each_bill = total_bill / divide_people
+final_amount = around(each_bill,2)
+print(f"Each person should pay {final_amount}")
