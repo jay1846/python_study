7fce9c5fc9421e1bcaddb93296169ad25db50058
jay1846 <jay9809012@gmail.com>
2026-06-10 10:36:07 +0200

    primitive data types

---
+++ Day2/primitive_data_types.py
@@ -0,0 +1,18 @@
+print("Hello"[0]) # H will be printed
+
+print("Hello"[-1]) # o will be printed (negative index)
+
+print("123"+"345") # Concatenation
+
+# Integer = Whole number
+print(123 + 345)
+
+# Large Integers
+print(123_456_789) # underline will be ignored
+
+# Float
+print(3.141592)
+
+# Boolean
+print(True)
+print(False)
