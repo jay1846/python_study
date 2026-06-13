643e54b322e33e0e08039de825ae55ff73773e5f
jay1846 <jay9809012@gmail.com>
2026-06-13 16:49:53 +0200

    docs: added new examples of indexing in nested list

--- Day4/nested_list.py
+++ Day4/nested_list.py
@@ -3,3 +3,9 @@ other_list = ['A', 'B', 'C']
 
 new_list = [my_list, other_list]   
 print(new_list)
+
+# first element of first indexed list ---> 'B'
+print(new_list[1][1])
+
+# first indexed list of new list -> other_list
+print(new_list[1])
