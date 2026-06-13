fb552f56302fda8a081eb4ca9a7317e21a0f808e
jay1846 <jay9809012@gmail.com>
2026-06-13 17:51:24 +0200

    usage of range function

---
+++ Day5/range_function.py
@@ -0,0 +1,14 @@
+# Range function with For loop
+
+for number in range(1,10):      # last number is not included
+    print(number)
+
+for number in range(1,11,3):
+    print(number)
+
+# Gaus's value
+sum = 0
+for num in range (1,101):
+    sum += num
+
+print(sum)
