c7e9483fefceb68e1f94d25ea2444467bcd168b5
jay1846 <jay9809012@gmail.com>
2026-07-26 11:37:43 +0200

    how to define and execute function

---
+++ Day6/functions.py
@@ -0,0 +1,10 @@
+print("Hello")
+num_char = len("Hello")        # because of parantheses we know that this is a function
+print(num_char)
+
+def my_function():              # define our own function
+    print("Hello")
+    print("Bye")
+
+
+my_function()
