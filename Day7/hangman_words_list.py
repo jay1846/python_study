c5de0103a5b6ca2ed22919920d4ebc81af7783e2
jay1846 <jay9809012@gmail.com>
2026-07-26 21:42:41 +0200

    to learn module (from..import..)

---
+++ Day7/hangman_words_list.py
@@ -0,0 +1,7 @@
+word_list = [
+    "aardvark", "baboon", "camel", "elephant", "giraffe",
+    "hippopotamus", "iguana", "jaguar", "kangaroo", "lion",
+    "monkey", "narwhal", "octopus", "penguin", "quokka",
+    "rhinoceros", "squirrel", "tiger", "unicorn", "vulture",
+    "walrus", "xenops", "yak", "zebra"
+]
